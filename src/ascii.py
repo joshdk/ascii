20fd8b1da3db6309433d928caf8248714b2e0e9d
Josh Komoroske <jdkomo.dev@gmail.com>
2013-01-21 16:17:20 -0500

    Major restructure
    - Reworked several functions to tak a PIL Image, and return a PIL Image.
    - Removed many (now) useless functions.

--- src/ascii.py
+++ src/ascii.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python2
-#{{{ Imports
 from __future__ import print_function, division
 from PIL import Image as img
 import sys
-#}}}
 
 
 
 
-
-def get_dimensions():
+#{{{ Take a PIL Image, and return a resized PIL Image
+def _resize(im):
 	import fcntl, termios, struct
-	return struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))
-
-
-#{{{ Reshape array
-def reshape(seq, size):
-	result = []
-	(height, width) = size
-	for i, item in enumerate(seq):
-		if not i < height*width:
-			break
-		if i%width == 0:
-			result.append([])
-		result[-1].append(item)
-	return result
-#}}}
-
-
-#{{{ Resize image to a specific (terminal) width
-def resize(image, size):
-	(height, width) = size
-	iheight = image.size[1]
-	iwidth  = image.size[0]
-
-	twidth = width
+	(theight, twidth) = struct.unpack('hh',  fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))
+	(iwidth, iheight) = im.size
 
 	nwidth = twidth
 	nheight = iheight / (iwidth/twidth)
@@ -42,42 +18,19 @@ def resize(image, size):
 	nwidth  = int(nwidth)
 	nheight = int(nheight)
 
-	return image.resize((nwidth, nheight))
+	return im.resize((nwidth, nheight))
 #}}}
 
 
-#{{{ Map each pixel based on a function
-def imgmap(image, fn):
-	height = len(image)
-	width  = len(image[0])
-	result = reshape([None for _ in range(height*width)], (height, width))
-	for y in range(height):
-		for x in range(width):
-			result[y][x] = fn(image[y][x])
-	return result
-#}}}
-
-
-#{{{ Print pixel matrix to the screen
-def render(image):
-	height = len(image)
-	width  = len(image[0])
-	for y in range(height):
-		for x in range(width):
-			print(image[y][x].encode('utf-8'), end='')
-		print('')
-#}}}
-
-
-#{{{ Grayscale conversion
-def grayscale(im):
+#{{{ Take a PIL Image, and return a grayscale PIL Image
+def _grayscale(im):
 	from numpy import array, uint8
 
 	im = im.convert('F')
 	data = array(im)
 
 	cmin, cmax = data.min(), data.max()
-	scale = 255 / (cmax-cmin or 1)
+	scale = 255.0 / (cmax-cmin or 1)
 
 	bytedata = ((data-cmin)*scale+0.499).astype(uint8)
 
@@ -85,20 +38,38 @@ def grayscale(im):
 #}}}
 
 
+#{{{ Take a PIL Image, and return ascii data
+def ascii(im, chars='  .,-:;!*=$#@'):
+	(width, height) = im.size
+	text = []
+	line = ''
+
+	for i, pixel in enumerate(im.getdata()):
+		pixel = pixel / 255.0
+		line += chars[int(pixel * len(chars) - (1 if pixel >= 1 else 0))]
+		# line += chars[int(pixel * len(chars))]
+		if i % width == width - 1:
+		# if i % width == 0 and i != 0:
+			text.append(line)
+			line = ''
+	return text
+#}}}
 
 
-#{{{ Run
-def run(image, width=80, charset='  .,-:;!*=$#@'):
-	normalize = lambda image: imgmap(image, lambda pixel: pixel/255)
-	charmap   = lambda image: imgmap(image, lambda pixel: charset[int(pixel * len(charset) - (1 if pixel>=1.0 else 0))])
+#{{{ Take ascii data, and display it on the screen
+def render(text, file=sys.stdout):
+	for y in range(len(text)):
+		for x in range(len(text[y])):
+			file.write(text[y][x])
+		file.write('\n')
+#}}}
 
-	image = resize(image, (None, width))
-	image = grayscale(image)
-	pixels = reshape(list(image.getdata()), (image.size[1], image.size[0]))
 
-	render(charmap(normalize(pixels)))
 
-	return True
+
+#{{{ Take a PIL Image, transform it, and return ascii data
+def _process(im, chars=u'  .,-:;!*=$#@'):
+	return ascii(_grayscale(_resize(im)), chars)
 #}}}
 
 
@@ -108,39 +79,29 @@ def main(argv=None):
 		argv = sys.argv
 	argc = len(argv)
 
-	if argc < 2:
+	if argc <= 1:
 		print('%s: error: insufficient arguments' % (argv[0]))
 		return 1
 
-	data = None
-	height, width = get_dimensions()
+	im = None
 	chars = u'  .,-:;!*=$#@'
 
-	if argc >= 4:
-		chars = argv[3]
+	if argc >= 2:
+		try:
+			im = img.open(argv[1])
+		except:
+			print('%s: error: could not open %s' % (argv[0], argv[1]))
+			return 1
+
+	if argc >= 3:
+		chars = argv[2]
 		chars = chars.decode('utf-8')
 		if len(chars) < 1:
 			print('%s: error: chars must contain at least one character' % (argv[0]))
 			return 1
 
-	if argc >= 3:
-		try:
-			width = int(argv[2])
-		except:
-			print('%s: error: width must be a positive, non-zero integer' % (argv[0]))
-			return 1
-		if width < 1:
-			print('%s: error: width must be a positive, non-zero integer' % (argv[0]))
-			return 1
-
-	if argc >= 2:
-		try:
-			image = img.open(argv[1])
-		except:
-			print('%s: error: could not open %s' % (argv[0], argv[1]))
-			return 1
-
-	run(image, width, chars)
+	text = _process(im, chars)
+	render(text)
 	return 0
 
 
