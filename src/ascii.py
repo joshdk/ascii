306a1b781e512beaf499d3ae6158ec98158426c9
Josh Komoroske <jdkomo.dev@gmail.com>
2013-03-05 16:15:27 -0500

    Added some color support
    Now takes an image and builds both a character map & a color map.

--- src/ascii.py
+++ src/ascii.py
@@ -11,6 +11,28 @@ import sys
 
 
 
+#{{{ Custom colors
+COLORS = [
+	(0x21, 0x21, 0x21),
+	(0xC7, 0x23, 0x41),
+	(0x85, 0xC6, 0x00),
+	(0xFF, 0x91, 0x00),
+	(0x33, 0x71, 0xBB),
+	(0x9B, 0x2A, 0x65),
+	(0x44, 0x6A, 0x6B),
+	(0xB0, 0xB0, 0xB0),
+	(0x48, 0x53, 0x56),
+	(0xFF, 0x5D, 0x4A),
+	(0xC2, 0xF4, 0x56),
+	(0xF6, 0x9D, 0x3C),
+	(0x68, 0x9A, 0xD6),
+	(0x9A, 0x4E, 0x76),
+	(0x76, 0xB2, 0xB3),
+	(0xE2, 0xE2, 0xE2)
+]
+#}}}
+
+
 #{{{ Resize image
 def _resize(im):
 	"""
@@ -73,30 +95,82 @@ def ascii(im, chars='  .,-:;!*=$#@'):
 	Take a PIL Image, and return an array of strings, that contain ascii data.
 	"""
 	(width, height) = im.size
-	text = []
-	line = ''
+	data = []
+	line = []
 
 	for i, pixel in enumerate(im.getdata()):
 		pixel = pixel / 255.0
-		line += chars[int(pixel * len(chars) - (1 if pixel >= 1 else 0))]
-		# line += chars[int(pixel * len(chars))]
+		line.append(chars[int(pixel * len(chars) - (1 if pixel >= 1 else 0))])
+
 		if i % width == width - 1:
-		# if i % width == 0 and i != 0:
-			text.append(line)
-			line = ''
-	return text
+			data.append(line)
+			line = []
+	return data
+#}}}
+
+
+#{{{ Partition image into color groups
+def color(im, colors):
+	"""
+	Take a PIL Image, and return an array of colors, that map to terminal colors.
+	"""
+	import math
+	im = im.convert('RGB')
+
+	(width, height) = im.size
+	data = []
+	line = []
+
+	for i, pixel in enumerate(im.getdata()):
+		cbest = None
+		for j, color in enumerate(colors):
+
+			cdist = (
+				(pixel[0] - color[0])**2 +
+				(pixel[1] - color[1])**2 +
+				(pixel[2] - color[2])**2
+			)
+
+			if cbest is None or cdist < cbest[0]:
+				cbest = (cdist, j)
+		line.append(cbest[1])
+
+		if i % width == width - 1:
+			data.append(line)
+			line = []
+	return data
 #}}}
 
 
 #{{{ Display ascii data
-def render(text, file=sys.stdout):
+def render(characters, colors, file=sys.stdout):
 	"""
-	Takes ascii data, and display it on the screen
+	Takes ascii & color data, and display it on the screen
 	"""
-	for y in range(len(text)):
-		for x in range(len(text[y])):
-			file.write(text[y][x])
+	import curses
+
+	curses.setupterm()
+	creset = curses.tigetstr('sc')
+	ccolor = curses.tigetstr('setaf')
+	cbold  = curses.tigetstr('bold')
+
+	def set_color(n):
+		if n in range(0, 8):
+			return curses.tparm(ccolor, n)
+		if n in range(8, 16):
+			return curses.tparm(ccolor, n) + curses.tparm(cbold)
+		return ''
+
+	def reset_colors():
+		return curses.tparm(creset)
+
+	for y in range(len(characters)):
+		for x in range(len(characters[y])):
+			file.write(set_color(colors[y][x]))
+			file.write(characters[y][x])
 		file.write('\n')
+
+	file.write(reset_colors())
 #}}}
 
 
@@ -132,7 +206,9 @@ def main(argv=None):
 			print('%s: error: chars must contain at least one character' % (argv[0]))
 			return 1
 
-	render(ascii(_grayscale(_resize(im)), chars))
+	rim = _resize(im)
+	render(ascii(_grayscale(rim), chars), color(rim, COLORS))
+
 	return 0
 
 
